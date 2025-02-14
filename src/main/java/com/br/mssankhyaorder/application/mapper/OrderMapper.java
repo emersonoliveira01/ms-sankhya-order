a6b60aae092d3b72cf9705f1a1afbfb4d27c2e36
emersonoliveira01 <emersongpi@hotmail.com>
2025-02-14 15:33:38 -0300

    remove imports

--- src/main/java/com/br/mssankhyaorder/application/mapper/OrderMapper.java
+++ src/main/java/com/br/mssankhyaorder/application/mapper/OrderMapper.java
@@ -2,14 +2,11 @@ package com.br.mssankhyaorder.application.mapper;
 
 import com.br.mssankhyaorder.application.dto.OrderRequest;
 import com.br.mssankhyaorder.application.dto.OrderResponse;
-import com.br.mssankhyaorder.application.dto.ProductResponse;
 import com.br.mssankhyaorder.domain.model.Order;
-import com.br.mssankhyaorder.domain.model.OrderProduct;
+
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
